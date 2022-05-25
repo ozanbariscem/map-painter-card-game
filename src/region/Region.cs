5e3cf7d8bb8c880bb62ab6eb2fbd8069c2fbc9c5
ozanbariscem <ozanbarscem@gmail.com>
2022-05-25 17:50:45 +0300

    region, region HUD, region manager map creation

--- src/region/Region.cs
+++ src/region/Region.cs
@@ -1,15 +1,75 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Region : Node2D
 {
-    public override void _Ready()
+    public static event Action<Region> OnReady;
+    public static event Action<Region> OnMouseEnter;
+    public static event Action<Region> OnMouseExit;
+
+    private static int idCount = 0;
+
+    [Export] public int Id;
+    // We can't export ulong arrays on Godot but i'm pretty sure we wont need an id bigger than 2,147,483,647
+    [Export] public int[] Neighbours = { 0 };
+
+    public Player Occupier { get; private set; }
+
+    public List<Card> Attackers { get; private set; }
+    public List<Card> Defenders { get; private set; }
+
+    private Area2D area;
+    private Sprite outline;
+    private RegionHUD hud;
+
+    public Region()
     {
-        
+        Id = idCount++;
+        Attackers = new List<Card>();
+        Defenders = new List<Card>();
     }
 
-    public override void _Process(float delta)
+    public override void _Ready()
     {
-        
+        GetNodes();
+        hud.SetRegion(this);
+
+        area.Connect("mouse_entered", this, nameof(OnMouseEntered));
+        area.Connect("mouse_exited", this, nameof(OnMouseExited));
+
+        OnReady?.Invoke(this);
+    }
+
+    public override void _ExitTree()
+    {
+        area.Disconnect("mouse_entered", this, nameof(OnMouseEntered));
+        area.Disconnect("mouse_exited", this, nameof(OnMouseExited));
+    }
+
+    public void SetNeighbours(int[] neighbours)
+    {
+        Neighbours = neighbours;
+    }
+
+    private void GetNodes()
+    {
+        outline = GetNode("visuals/outline") as Sprite;
+        area = GetNode("area") as Area2D;
+        hud = GetNode("hud") as RegionHUD;
+    }
+
+    private void OnMouseEntered()
+    {
+        outline.Visible = true;
+        ZIndex = 1;
+        OnMouseEnter?.Invoke(this);
+    }
+
+    private void OnMouseExited()
+    {
+        outline.Visible = false;
+        ZIndex = 0;
+        OnMouseExit?.Invoke(this);
     }
 }
---
+++ src/region/ui/RegionHUD.cs
@@ -0,0 +1,70 @@
+﻿using Godot;
+using System;
+
+public class RegionHUD : Control
+{
+    public enum State { Stable, Transition }
+    
+    public static float transitionSpeed = 5f;
+
+    private Label nameLabel;
+
+    private int transitionDirection;
+    private State state;
+    private Region region;
+
+    public override void _Ready()
+    {
+        Modulate = new Color(1, 1, 1, 0);
+        nameLabel = GetNode("Name") as Label;
+
+        Region.OnMouseEnter += HandleMouseEnter;
+        Region.OnMouseExit += HandleMouseExit;
+    }
+
+    public override void _ExitTree()
+    {
+        Region.OnMouseEnter -= HandleMouseEnter;
+        Region.OnMouseExit -= HandleMouseExit;
+    }
+
+    public override void _Process(float delta)
+    {
+        Transition(delta);
+    }
+
+    public void SetRegion(Region region)
+    {
+        this.region = region;
+        nameLabel.Text = region.Name;
+    }
+
+    private void Transition(float delta)
+    {
+        if (state == State.Stable) return;
+
+        Modulate = new Color(Modulate.r, Modulate.g, Modulate.b, Modulate.a + transitionDirection * transitionSpeed * delta);
+        if (state == State.Transition)
+        {
+            if (Modulate.a <= 0 || Modulate.a >= 1)
+            {
+                Modulate = new Color(1, 1, 1, Modulate.a < 1 ? 0 : 1);
+                state = State.Stable;
+            }
+        }
+    }
+
+    private void HandleMouseEnter(Region region)
+    {
+        if (this.region != region) return;
+        state = State.Transition;
+        transitionDirection = 1;
+    }
+
+    private void HandleMouseExit(Region region)
+    {
+        if (this.region != region) return;
+        state = State.Transition;
+        transitionDirection = -1;
+    }
+}
